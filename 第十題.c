08dfaeee9eccbae6c1bb4bdb66e2f6d593ffe3b5
Simercher <62394051+Simercher@users.noreply.github.com>
2021-09-27 17:29:15 +0800

    Add files via upload

--- 第十題.c
+++ 第十題.c
@@ -5,10 +5,10 @@ int main()
 {
     int x, y;
     scanf("%d%d", &x, &y);
-    printf("%d+%d=%d", x, y, x + y);
-    printf("%d*%d=%d", x, y, x * y);
-    printf("%d-%d=%d", x, y, x - y);
-    printf("%d/%d=%d...%d", x, y, x / y, x % y);
+    printf("%d+%d=%d\n", x, y, x + y);
+    printf("%d*%d=%d\n", x, y, x * y);
+    printf("%d-%d=%d\n", x, y, x - y);
+    printf("%d/%d=%d...%d\n", x, y, x / y, x % y);
 
     return 0;
 }
